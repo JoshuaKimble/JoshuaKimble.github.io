1330b5f956bfdbefb7022195af14dc6e01522142
Joshua <joshuakimble@overstock.com>
2024-05-04 14:07:28 -0600

    Add basic version of kiddie-coins app

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,36 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	let people = [
+		{ name: 'Everett', coins: 0 },
+		{ name: 'Ellison', coins: 0 },
+		{ name: 'River', coins: 0 }
+	];
+
+	function addCoin(person) {
+		person.coins += 1;
+	}
+</script>
+
+<div class="container">
+	<h1>Add Coins</h1>
+	{#each people as person}
+		<div class="person">
+			<span>{person.name}: {person.coins} coins</span>
+			<button on:click={() => addCoin(person)}>Add Coin</button>
+		</div>
+	{/each}
+</div>
+
+<style>
+	.container {
+		display: flex;
+		flex-direction: column;
+		align-items: flex-start;
+		padding: 20px;
+	}
+	.person {
+		margin-bottom: 10px;
+	}
+	button {
+		margin-left: 10px;
+	}
+</style>
